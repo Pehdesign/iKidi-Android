350b1b0839a6ea492076e5e9ce94f210265c2ab6
pawelpalacz <Pehdesign@outlook.com>
2017-10-12 16:19:18 +0200

    Adjust small things

--- app/src/main/java/com/ikidi/pehdesign/ikidi_android/SettingsActivity.java
+++ app/src/main/java/com/ikidi/pehdesign/ikidi_android/SettingsActivity.java
@@ -130,7 +130,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
